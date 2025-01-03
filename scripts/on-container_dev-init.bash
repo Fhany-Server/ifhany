46bbfe1aa7cd02ecf5a071b10a6c7b7aa694b96b
Myna <contact@devmyna.xyz>
2025-01-03 16:48:35 -0300

    chore: implement Rust to the dev environment (#44)

--- scripts/on-container_dev-init.bash
+++ scripts/on-container_dev-init.bash
@@ -3,12 +3,16 @@ case "$1" in
     "--no-build")
     ;;
     *)
-        cargo run --bin prisma generate
+        #cargo run --bin prisma -- generate
     ;;
 esac
 
 rustup default stable
 
-cargo build
+#cargo build
 
-lldb-server g 0.0.0.0:2129 -- /app/target/debug/ifhany
+#lldb-server g 0.0.0.0:2129 -- /app/target/debug/ifhany
+
+while true; do
+    sleep 5;
+done
