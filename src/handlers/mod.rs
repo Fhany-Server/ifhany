ba2b51fb46aed4d125cb5637f009a27624c3d15d
Myna <contact@devmyna.xyz>
2025-01-03 16:49:49 -0300

    feat: add supported locales (#52)

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -2,3 +2,4 @@ pub mod command;
 pub mod event;
 pub mod error;
 pub mod punishment;
+pub mod i18n;
--- src/utils/values/enums.rs
+++ src/utils/values/enums.rs
@@ -1,6 +1,16 @@
+use strum_macros::{EnumIter, Display};
+
 #[derive(PartialEq, Debug)]
 pub enum EmojiFormat {
     Unicode,
     Custom,
     Discord
+}
+
+#[derive(EnumIter, Display)]
+pub enum SupportedLocales {
+    #[strum(serialize = "en-US")]
+    EnUs,
+    #[strum(serialize = "pt-BR")]
+    PtBr,
 }
